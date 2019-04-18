ab3bc1be1ee4950806861bc7e308d3feae300045
VanDeny <badura.denis@gmail.com>
2019-04-18 10:22:22 +0200

    Added Google Map API to get Lat. and Long. from city

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,4 @@
+/*
 function getNow(lattitude, longtitude)
 {
     var url = "https://api.darksky.net/forecast/482e27318ded58f995089bfdccebd10c/" + lattitude + "," + longtitude + "?exclude=daily,hourly,alerts,flags,minutely";
@@ -12,4 +13,33 @@ function getNow(lattitude, longtitude)
     }).fail(function(err){
         alert("Someting went wrong: " + err.responseTest);
     });
+}
+*/
+
+var Location = {
+    name : String,
+    latitude : String,
+    longitude : String,
+    temperature : String,
+    feelTemp : String,
+    icon : String
+}
+
+
+function getLatLong(location)
+{
+    var geocoder =  new google.maps.Geocoder();
+    geocoder.geocode( { 'address': location }, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            //TODO Add to the Location and Check if is already there
+        } else {
+            alert("Something got wrong " + status);
+        }
+    });
+}
+
+
+function addCity()
+{
+    document.getElementById('addCityDialog').show();
 }
