06f7ad180c51834cae0f05f33377695cff8d22e1
VanDeny <badura.denis@gmail.com>
2019-04-22 14:12:58 +0200

    Metasave

--- js/javascript.js
+++ js/javascript.js
@@ -11,20 +11,18 @@ var Location = {
 var listOfLocations = [];
 var res;
 
-function getWeatherNow(lattitude, longtitude)
+function getWeatherNow(latitude, longtitude)
 {
-    var urls = "https://api.darksky.net/forecast/482e27318ded58f995089bfdccebd10c/" + lattitude + "," + longtitude + "?exclude=daily,hourly,alerts,flags,minutely";
-    $.ajax({
-        url:$("#url").val(),
-        type: "GET",
-        data:{"currently": $("#currently")},
-        success:function(data)
-        {
-            alert("Success " + data);
-        }
-    }).fail(function(err){
-        alert("Someting went wrong: " + err.responseTest);
-    });
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    var urls = '${proxy}https://api.darksky.net/forecast/482e27318ded58f995089bfdccebd10c/${latitude},${longtitude}?exclude=daily,hourly,alerts,flags,minutely';
+    fetch(urls)
+        .then(response=> {
+            return response.json();
+        })
+        .then(data =>{
+            console.log(data);
+        })
+
 }
 
 
